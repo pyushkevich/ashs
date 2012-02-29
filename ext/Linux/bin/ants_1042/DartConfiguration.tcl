b4e43670a5c0848c56ebdb39d947084a2e0f95fa
yushkevich <yushkevich@811b3240-e085-0410-ad0e-e541e759bf7c>
2012-02-29 18:51:05 +0000

    Updated the linux directory
    git-svn-id: https://www.nitrc.org/svn/ashs@68 811b3240-e085-0410-ad0e-e541e759bf7c

---
+++ ext/Linux/bin/ants_1042/DartConfiguration.tcl
@@ -0,0 +1,90 @@
+# This file is configured by CMake automatically as DartConfiguration.tcl
+# If you choose not to use CMake, this file may be hand configured, by
+# filling in the required variables.
+
+
+# Configuration directories and files
+SourceDirectory: /mnt/build/pauly/ants/advants/Examples
+BuildDirectory: /mnt/build/pauly/ants/gcc64rel
+
+# Where to place the cost data store
+CostDataFile: 
+
+# Site is something like machine.domain, i.e. pragmatic.crd
+Site: picsl-build.uphs.upenn.edu
+
+# Build name is osname-revision-compiler, i.e. Linux-2.4.2-2smp-c++
+BuildName: Linux-c++
+
+# Submission information
+IsCDash: TRUE
+CDashVersion: 
+QueryCDashVersion: 
+DropSite: picsl.upenn.edu
+DropLocation: /cdash/submit.php?project=ANTS
+DropSiteUser: 
+DropSitePassword: 
+DropSiteMode: 
+DropMethod: http
+TriggerSite: 
+ScpCommand: /usr/bin/scp
+
+# Dashboard start time
+NightlyStartTime: 00:00:00 EST
+
+# Commands for the build/test/submit cycle
+ConfigureCommand: "/mnt/build/liuyang/pkg/cmake-2.8.4/bin/cmake" "/mnt/build/pauly/ants/advants/Examples"
+MakeCommand: /usr/bin/gmake -i
+DefaultCTestConfigurationType: Release
+
+# CVS options
+# Default is "-d -P -A"
+CVSCommand: /usr/bin/cvs
+CVSUpdateOptions: -d -A -P
+
+# Subversion options
+SVNCommand: /usr/bin/svn
+SVNUpdateOptions: 
+
+# Git options
+GITCommand: /usr/local/bin/git
+GITUpdateOptions: 
+GITUpdateCustom: 
+
+# Generic update command
+UpdateCommand: /usr/bin/svn
+UpdateOptions: 
+UpdateType: svn
+
+# Compiler info
+Compiler: /usr/bin/c++
+
+# Dynamic analysis and coverage
+PurifyCommand: 
+ValgrindCommand: 
+ValgrindCommandOptions: 
+MemoryCheckCommand: /usr/bin/valgrind
+MemoryCheckCommandOptions: 
+MemoryCheckSuppressionFile: 
+CoverageCommand: /usr/bin/gcov
+
+# Cluster commands
+SlurmBatchCommand: SLURM_SBATCH_COMMAND-NOTFOUND
+SlurmRunCommand: SLURM_SRUN_COMMAND-NOTFOUND
+
+# Testing options
+# TimeOut is the amount of time in seconds to wait for processes
+# to complete during testing.  After TimeOut seconds, the
+# process will be summarily terminated.
+# Currently set to 25 minutes
+TimeOut: 1500
+
+UseLaunchers: 
+CurlOptions: 
+# warning, if you add new options here that have to do with submit,
+# you have to update cmCTestSubmitCommand.cxx
+
+# For CTest submissions that timeout, these options
+# specify behavior for retrying the submission
+CTestSubmitRetryDelay: 5
+CTestSubmitRetryCount: 3
